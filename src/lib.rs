50591d15dbac0a2cd1b032767a28fa63f95e382c
$tatu$ <118591147+6j4w@users.noreply.github.com>
2024-01-30 14:42:33 -0800

    Everything is working

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use std::{ fs, io };
+use rand::Rng;
+
 #[derive(PartialEq)]
 pub enum Status {
     FOUND,
@@ -33,11 +36,15 @@ pub static ALPHA_LOWER: [char; 26] = [
     'z',
 ];
 
+// basic structure for the game
 pub struct GameData {
     pub lives: usize,
     pub found_letters: usize,
+    pub wrong_letters: Vec<char>,
+    pub guessed_letters: Vec<char>,
 
     pub ended: bool,
+    pub ended_msg: String,
 
     pub word_readable: String,
     pub word: Vec<(char, Status)>,
@@ -45,4 +52,65 @@ pub struct GameData {
 
 pub fn clear_screen() {
     print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+}
+
+pub fn get_input() -> char {
+    let mut guess: String = "".to_string();
+
+    print!("Guess: ");
+    io::stdin().read_line(&mut guess).expect("Input went wrong");
+
+    guess.trim().to_lowercase().chars().next().expect("Input went wrong")
+}
+
+pub fn build_game_data() -> GameData {
+    let word = get_readable();
+    GameData {
+        lives: 9,
+        found_letters: 0,
+        wrong_letters: Vec::new(), // <- this is for letters that were  wrong
+        guessed_letters: Vec::new(),
+
+        ended: false,
+        ended_msg: "You Lose.".to_string(),
+
+        word_readable: word.clone(),
+        word: readable_to_word(word),
+    }
+}
+
+pub fn check_ended(game_data: &mut GameData) {
+    if game_data.word.iter().all(|(_, status)| *status == Status::FOUND) {
+        game_data.ended = true;
+        game_data.ended_msg = "You Win!".to_string();
+    } else if game_data.lives == 0 {
+        game_data.ended = true;
+        game_data.ended_msg = "You lose...".to_string();
+    }
+}
+
+pub fn outro(game_data: &GameData) {
+    clear_screen();
+    println!("\n{0}", game_data.ended_msg);
+    println!("The word was: {0}", game_data.word_readable);
+    println!("You have {} lives out of 9 left", game_data.lives);
+    println!("Good game!");
+}
+
+fn get_readable() -> String {
+    let contents = fs::read_to_string("words.dic").expect("Wordlist not readable");
+    let mut lines = contents.lines();
+    let rand_num = rand::thread_rng().gen_range(0..lines.clone().count());
+
+    lines.nth(rand_num).expect("Retreiving random word didn't word").to_string()
+}
+
+fn readable_to_word(word_readable: String) -> Vec<(char, Status)> {
+    let mut output: Vec<(char, Status)> = vec![];
+
+    for c in word_readable.chars() {
+        output.push((c, Status::NOTFOUND));
+    }
+
+    output
 }
--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,60 @@
-use hangman::{ Status, ALPHA_LOWER, GameData, clear_screen };
-use std::io;
+use hangman::{ build_game_data, check_ended, outro, clear_screen, get_input, Status, ALPHA_LOWER };
 
 fn main() {
-    let mut guess: String = String::new();
-    let mut exit_mess: String = "You Lose!".to_string();
-    //let mut game_data.lives: bool = false;
-    let mut game_data = GameData {
-        lives: 9,
-        found_letters: 0,
-        ended: false,
-        word_readable: "heelinge".to_string(),
-        word: vec![
-            ('h', Status::NOTFOUND),
-            ('e', Status::NOTFOUND),
-            ('e', Status::NOTFOUND),
-            ('l', Status::NOTFOUND),
-            ('i', Status::NOTFOUND),
-            ('n', Status::NOTFOUND),
-            ('g', Status::NOTFOUND),
-            ('e', Status::NOTFOUND)
-        ],
-    };
+    let mut guess: char;
+    let mut err: &str = "";
+
+    let mut game_data = build_game_data();
 
     println!("Welcome! Let's play hangman!");
     loop {
         let found_letters_old = game_data.found_letters;
-        clear_screen();
-        println!("Lives: {}", game_data.lives);
-        for (char, status) in &game_data.word {
-            print!("{} ", if *status == Status::FOUND { char } else { &'_' });
-        }
+
+        check_ended(&mut game_data);
+
         if game_data.ended {
-            clear_screen();
-            println!("\n{exit_mess}");
-            println!("The word was: {0}", game_data.word_readable);
-            println!("You have {} lives out of 9 left", game_data.lives);
-            println!("Good game!");
+            outro(&game_data);
             break;
         }
 
-        println!("\n\nGuess: ");
-        io::stdin().read_line(&mut guess).expect("Input went wrong");
-        let first_char = guess.trim().to_lowercase().chars().next().unwrap();
+        clear_screen();
 
-        if !ALPHA_LOWER.contains(&first_char) {
+        println!("\n{}", err);
+        err = "";
+
+        println!("Lives: {}", game_data.lives);
+
+        for (char, status) in &game_data.word {
+            // e.g. "_ e _ l"
+            print!("{} ", if *status == Status::FOUND { char } else { &'_' });
+        }
+
+        println!("\nYou have guessed: {:?} ", game_data.wrong_letters);
+        println!("\n\nGuess: ");
+
+        guess = get_input();
+
+        if !ALPHA_LOWER.contains(&guess) {
             eprintln!("Not a valid character.");
         }
+
+        if game_data.guessed_letters.iter().any(|c| c == &guess) {
+            err = "You already guessed this!";
+            continue;
+        }
+
         for (c, status) in &mut game_data.word {
-            if *c == first_char {
+            if *c == guess {
                 *status = Status::FOUND;
                 game_data.found_letters += 1;
             }
         }
 
-        game_data.lives -= if found_letters_old == game_data.found_letters { 1 } else { 0 };
-        game_data.ended = if game_data.lives == 0 { true } else { false };
-
-        if game_data.word.iter().all(|(_, status)| *status == Status::FOUND) {
-            exit_mess = "You win!".to_string();
-            game_data.ended = true;
-        }
-
-        guess = "".to_string();
+        game_data.lives -= if found_letters_old == game_data.found_letters {
+            game_data.wrong_letters.push(guess);
+            1
+        } else {
+            0
+        };
     }
 }
